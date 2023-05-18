e0c183102965fc8c84b2f30685bfcfd5e3f9fad1
tariq khan <tariqjameskhan@hotmail.co.uk>
2023-05-18 15:08:30 +0100

    completed rspec

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
 end
